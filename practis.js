0adac2e96988830a51ccd05d5d6848a6ae7b1a32
abhirajsingh524 <thakurarpitsingh524@gmail.com>
2026-09-16 20:25:24 +0530

    practice qustion from  to 10

---
+++ practis.js
@@ -0,0 +1,83 @@
+// console.log("to print the numbers from 0 to 100");
+//  for (let i = 0; i <= 100; i++) {
+//     console.log(i);
+// }   
+
+//console.log("to print the numbers from 100 to 0 in reverse order");
+// let i =100;
+
+// while(i>=0){
+//     console.log(i);
+//    i--;
+    
+// }
+
+//console.log("to print the even numbers from 1 to 50")
+// for(let i=1;i<=50;i++){
+//     if(i%2==0){
+//     console.log("even numbers:" + i);
+//     }
+   
+// }
+
+//console.log("print odd number from 1 to 50 ")
+// for(let i=1;i<50;i++){
+//     if(i%2!=0){
+//         console.log("odd numbers:" + i);
+//     }
+// }
+//console.log("print the sum of numbers form 1 to n numbers");
+
+// let n=10;
+//  let sum=0;
+// for(let i=1;i<=n;i++){
+//    sum+=i;
+    
+// }
+// console.log("sum of numbers from 1 to n:" + sum);
+// console.log("find the factorial of n ");
+// let n=6
+// let fact=1;
+// for(let i=1;i<=n;i++){
+//     fact*=i;
+// }
+// console.log("factorial of " + n + " is: " + fact);
+
+//consle.log("print the multiplication table of n");
+// let n=5;
+// for(let i=1;i<=10;i++){
+//     console.log(n +"*"+i+"="+(n*i));
+// }
+// console.log("count how many numbers are in positive integers" );
+// let arr=[1,2,3,-4,-5,6,7,-8,9];
+// let count=0;
+// for(let i=0;i<arr.length;i++){
+//    if(arr[i]>0){
+//     count++;
+//    }
+// }
+// console.log("count of positive numbers in the array is:"+count)
+// console.log("reverse the positive interger in a arry");
+// let arr=[1,2,3,-4,-5,6,7,-8,9,5,8,-5];
+// let positiveArr=[];
+// for(let i=arr.length;i>0;i--){
+//     if(arr[i]>0){
+//         positiveArr.push(arr[i]);
+
+//     }
+
+// }
+// positiveArr.sort((a,b)=>b-a);
+// console.log("positive numbers in the array are:"+positiveArr);
+// console.log("find the largest number in a number");
+// let n=89567;
+// let largest=0;
+// while(n>0){
+//     let rem=n%10;
+//     if(rem>largest){
+//         largest=rem;
+//     }
+
+// n=Math.floor(n/10);
+// }
+// console.log("largest number in the number is:" + largest);
